549ddf16769cdc41e173a43ab2bd43f8cb0e954e
oddynuff <katemc19@mail.com>
2024-03-28 20:39:44 +0200

    Complete task2.a + screens

---
+++ code/task2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Подсчет слов и символов</title>
+</head>
+<body>
+<form method="post">
+    <textarea name="text"></textarea>
+    <br>
+    <input type="submit" name="submit" value="Подсчитать">
+</form>
+<?php
+if(isset($_POST['submit']))
+{
+    $text = $_POST['text'];
+    $wordCount = str_word_count($text);
+    $charCount = strlen($text);
+
+    echo "<p>Количество слов: $wordCount</p>";
+    echo "<p>Количество символов: $charCount</p>";
+}
+?>
+</body>
