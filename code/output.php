500a9f295ca8b6938e05b4316b4dc16c059564e8
oddynuff <katemc19@mail.com>
2024-03-29 11:59:14 +0200

    complete task3 + screen + categories

--- code/output.php
+++ code/output.php
@@ -1,11 +1,22 @@
 <?php
-session_start();
-if(isset($_SESSION['userData']))
+function redirectToHome()
 {
-    echo '<ul>';
-    foreach ($_SESSION['userData'] as $key => $value)
-    {
-        echo "<li>$key: $value</li>";
-    }
-    echo '</ul>';
+    header("Location: task3.php");
+    exit();
 }
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description']))
+{
+    redirectToHome();
+}
+
+$category = $_POST['category'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+$category_folder = "./categories/$category";
+$file_path = "$category_folder/$title.txt";
+$content = "$email\n$title\n$desc";
+file_put_contents($file_path, $content);
+redirectToHome();
+?>
